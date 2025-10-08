ff956bd033a8a4000c534d21ea49766e1ac7e919
zeycodee <zecodee@gmail.com>
2025-10-08 10:30:03 +0800

    Update Rabu, 8 Oktober 2025

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("Hello Teman Teman!")
+import csv
+from pwinput import pwinput
+
+def login():
+    username = input("Input username: ")
+    password = pwinput("Input password: ")
+
+    with open("users.csv", mode="r") as file:
+        reader = csv.DictReader(file)
+        for user in reader:
+            if user["username"] == username and user["password"] == password:
+                print(f"Selamat datang, {user['nama_lengkap']}, sebagai {user['role']}!")
+                return user
+        
+        print("Login failed! username atau password salah!")
+        return None
+            
+def menu_dosen():
+    print("Menu Dosen")
+
+def menu_mhs():
+    print("Menu Mahasiswa")
+
+def mulai():
+    auth = login()
+
+    if auth:
+        if auth["role"] == "dosen":
+            menu_dosen()
+        elif auth["role"] == "mahasiswa":
+            menu_mhs()
+        else:
+            print("Role Anda tidak valid!")
+
+mulai()
